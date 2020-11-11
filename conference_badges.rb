c33777179d0aebd763e14031dfd2230c9818f752
Armando <rojas.armani@gmail.com>
2020-11-11 10:20:16 -0500

    commit tree e929e97d6dd786356fb281ada40ba8fd1288c6c1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    batch_badge = []
+    array.each do |name|
+        batch_badge << "Hello, my name is #{name}."
+    end
+    return batch_badge
+end
+
+def assign_rooms(array)
+    rooms = []
+    array.each_with_index do |name, room_assignments|
+        rooms << "Hello, #{name}! You'll be assigned to room #{room_assignments + 1}!"
+    end
+    return rooms
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |badge|
+        puts badge
+    end
+    assign_rooms(array).each do |assign|
+        puts assign
+    end
+end
